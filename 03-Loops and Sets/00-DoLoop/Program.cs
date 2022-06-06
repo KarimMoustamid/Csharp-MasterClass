1a0807a58b5f8b0c2f91b23ab1fd4ee582c2ec8a
Moustamid <Karim1992moustamid@gmail.com>
2022-06-06 11:25:37 -0500

    Basis Revisited

---
+++ 03-Loops and Sets/00-DoLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿bool isValidAge;
+int age;
+
+
+// Runs the Code at least one time .    
+do
+{
+    Console.WriteLine("What is your Age : ");
+    string ageText = Console.ReadLine();
+
+    isValidAge = int.TryParse(ageText, out age);
+    if (!isValidAge)
+    {
+        Console.WriteLine("That was an invalid age.");
+        // return;
+    }
+} while (isValidAge == false);
+
+Console.WriteLine($"Your age id {age}.");
+
+// while (true)
+{
+// Runs the Code 0 or more times     
+}
---
+++ 03-Loops and Sets/01-Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] firstNames = new string[5];
+
+firstNames[0] = "Tim";
+firstNames[1] = "Karim";
+firstNames[2] = "Lora";
+firstNames[3] = "Jonas";
+firstNames[4] = "Stephen";
+
+for (var i = 0; i < firstNames.Length; i++)
+{
+    Console.WriteLine(firstNames[i]);
+}
+
+string data = "Tim,Sue,Bob,Jane,Franck";
+
+string[] firstNamesData = data.Split(",");
+
+for (var i = 0; i < firstNamesData.Length; i++)
+{
+    Console.WriteLine(firstNamesData[i]);
+}
