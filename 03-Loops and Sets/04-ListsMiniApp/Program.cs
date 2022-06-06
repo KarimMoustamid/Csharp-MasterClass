3eaf2c8a0d1d71025face5ebe286818b6b4dabdb
Moustamid <Karim1992moustamid@gmail.com>
2022-06-06 14:21:33 -0500

    List Mini app completed

---
+++ 03-Loops and Sets/04-ListsMiniApp/Program.cs
@@ -0,0 +1,21 @@
+﻿List<string> rosterClassStudenys = new List<string>();
+bool addMoreStudents = true;
+
+
+do
+{
+    Console.WriteLine("Add a Student name to the Roster Class :");
+
+    var studentName = Console.ReadLine();
+    rosterClassStudenys.Add(studentName);
+
+    Console.Write("Do you want to Add More Students to The Class ? Y or N : ");
+    var answer = Console.ReadLine();
+    
+    if (answer.ToUpper() == "Y")
+    {
+        addMoreStudents = true;
+    }
+    else
+        addMoreStudents = false;
+} while (addMoreStudents);
