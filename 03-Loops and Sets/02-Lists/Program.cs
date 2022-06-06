21989e4558a251e5e1a54670c53dcd76fb0ff498
Moustamid <Karim1992moustamid@gmail.com>
2022-06-06 13:10:49 -0500

    Lists

---
+++ 03-Loops and Sets/02-Lists/Program.cs
@@ -0,0 +1,21 @@
+﻿
+List<string> colors = new List<string>();
+
+colors.Add("black");
+colors.Add("Red");
+colors.Add("Green");
+colors.Add("White");
+
+Console.WriteLine(colors[^1]);
+Console.WriteLine(colors[colors.Count - 1]);
+
+foreach(var color in colors)
+{
+    Console.WriteLine(color);
+}
+
+// List<T> - generic 
+
+string data = "Corey,Smith,Karim";
+var lastNames = data.Split(",").ToList(); 
+lastNames.Add("Jonas");
