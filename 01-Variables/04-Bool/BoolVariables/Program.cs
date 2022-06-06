186a52c05be5a123cde6c8a1ef5c94c1955ff0ed
Moustamid <Karim1992moustamid@gmail.com>
2022-06-06 11:30:31 -0500

    Basics Revisited

--- 01-Variables/04-Bool/BoolVariables/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-// true or false
-
-bool isComplete = true;
-
-//isComplete = !isComplete;
-
-Console.WriteLine(!isComplete);
-Console.WriteLine(isComplete);
--- 01-Variables/08-TimeOnly/TimeOnlyVariables/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿
-
-TimeOnly opensAt = TimeOnly.Parse("8:00 AM");
-
-TimeOnly rightNow = TimeOnly.FromDateTime(DateTime.Now);
-
-Console.WriteLine(opensAt);
-Console.WriteLine(rightNow);
